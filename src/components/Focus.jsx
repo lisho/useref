c756278d72023f65d9382aa9fcce3d9ceeda66dd
Lisho <lshevoweb@gmail.com>
2021-07-24 18:54:17 +0200

    firstCommit

---
+++ src/components/Focus.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Focus = () => {
+    const [texto, setTexto] = useState("");
+    const inputRef = useRef()
+ /*  useEffect(() => {
+      console.log(texto)
+      
+  }, [texto]) */
+
+  const handleFocaliza = () => {
+    inputRef.current.focus();
+  }
+
+  return (
+    <div>
+      <h1>Focus</h1>
+      <input
+        type="text"
+        value={texto}
+        onChange={(e) => setTexto(e.target.value)}
+        ref={inputRef}
+      />
+      <button onClick={handleFocaliza}>Focaliza!</button>
+    </div>
+  );
+};
+
+export default Focus;
